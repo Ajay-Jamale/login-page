c8d2a8a803c46b3f97b1ed320a14ab3b9f2392f3
Ajay-Jamale <ajayjamale90@gmail.com>
2024-10-27 22:24:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,3 +15,18 @@ const showHiddenPass = (loginPass, loginEye) => {
         }
     });
 };
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+    
+    if (e.key === 'F12') {
+        e.preventDefault();
+    }
+});
+document.addEventListener('keydown', function(e) {
+    if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.key === 'I') {
+    }
+});
